de7fc56175ea2040bb2627746448cab8e09ca059
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-05-13 15:05:27 -0500

    Add venta controler and VentaRequest

---
+++ WSVenta/Controllers/VentaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+using WSVenta.Models.Request;
+using WSVenta.Models.Response;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VentaController : ControllerBase
+    {
+        public IActionResult Add(VentaRequest ventaRequest)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (CventaRealContext bd = new CventaRealContext())
+                {
+                    //var venta = new Ventum();
+                    //venta.Total = ventaRequest.Total;
+                }    
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ WSVenta/Models/Request/VentaRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace WSVenta.Models.Request
+{
+    public class VentaRequest
+    {
+        public VentaRequest() {
+            this.Conceptos = new List<Concepto>();
+        }
+        public int IdCliente { get; set; }
+        public decimal Total{ get; set; }
+        public List<Concepto> Conceptos{ get; set; }
+
+
+    }
+    public class Concepto
+    {
+        public Concepto() { }
+        public int Cantidad{ get; set; }
+        public decimal PrecioUnitario{ get; set; }
+        public decimal Importe{ get; set; }
+        public int IdProducto{ get; set; }
+    }
+}
