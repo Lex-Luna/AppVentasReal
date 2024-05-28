3fd396137c11549774a1e572f54be6ff41732300
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-05-28 10:13:15 -0500

    add required in ClienteRequest

--- WSVenta/Models/Request/VentaRequest.cs
+++ WSVenta/Models/Request/VentaRequest.cs
@@ -1,12 +1,20 @@
-﻿namespace WSVenta.Models.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSVenta.Models.Request
 {
     public class VentaRequest
     {
         public VentaRequest() {
             this.Conceptos = new List<Concepto>();
         }
+        [Required]
+        [Range(1,Double.MaxValue, ErrorMessage ="El Id del cliente debe ser mayor a 0 hp")]
+        [ExisteClienteAtribute(ErrorMessage ="El cliente no existe")]
         public int IdCliente { get; set; }
+        
         public decimal Total{ get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage ="Minimo debe haber un concepto")]
         public List<Concepto> Conceptos{ get; set; }
 
 
@@ -20,4 +28,19 @@
         public decimal Importe{ get; set; }
         public int IdProducto{ get; set; }
     }
+    #region Validaciones
+    public class ExisteClienteAtribute: ValidationAttribute {
+        public override bool IsValid(object? value)
+        {
+            long idCliente=(long)value;
+            using (var bd = new Models.CventaRealContext())
+            {
+                if (bd.Clientes.Find(idCliente) == null) return false;
+                
+            }
+                return true;
+        }
+    }
+
+    #endregion
 }
